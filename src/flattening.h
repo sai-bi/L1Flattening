28b97ddc2b882cd26c8b28dc740dedc55644aed3
soundsilence <soundsilencebisai@gmail.com>
2015-09-23 16:23:09 -0700

    add: building sp mat

--- src/flattening.h
+++ src/flattening.h
@@ -3,11 +3,12 @@
 #include <Eigen/sparse>
 #include "cv.h"
 #include <opencv2/opencv.hpp>
+#include <vector>
 
 typedef Eigen::SparseMatrix<double> SpMat;
-typedef Eigen::Triplet<double> Trip;
+typedef Eigen::Triplet<double> Triple;
 
-SpMat BuildWindowVariationMatrix(const cv::Mat_<cv::Vec3b>& image,
+SpMat& BuildWindowVariationMatrix(const cv::Mat_<cv::Vec3b>& image,
 	int window_size);
 
 #endif
